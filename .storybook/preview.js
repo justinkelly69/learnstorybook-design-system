19ca0d6832eba2362fd9b8b2181dc6808a3cd1db
Justin Kelly <justinkelly@justinkelly.net>
2022-03-28 21:22:59 +0200

    added a11y

--- .storybook/preview.js
+++ .storybook/preview.js
@@ -16,12 +16,27 @@ export const decorators = [
   ),
 ];
 
-export const parameters = {
+/* export const parameters = {
   actions: { argTypesRegex: "^on[A-Z].*" },
   controls: {
     matchers: {
       color: /(background|color)$/i,
       date: /Date$/,
     },
+  },
+}; */
+
+/*
+ * More on Storybook global parameters at:
+ * https://storybook.js.org/docs/react/writing-stories/parameters#global-parameters
+ */
+export const parameters = {
+  actions: { argTypesRegex: "^on[A-Z].*" },
+  // Storybook a11y addon configuration
+  a11y: {
+    // the target DOM element
+    element: "#root",
+    // sets the execution mode for the addon
+    manual: false,
   },
 };
